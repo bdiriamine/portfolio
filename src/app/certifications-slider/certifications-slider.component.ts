98714882324759c641dbc06175c65450411526a7
aminebdiri <bdiri.amine08@gmail.com>
2024-11-28 09:36:25 +0100

    portfolio v0.2 edit  image uploads

--- src/app/certifications-slider/certifications-slider.component.ts
+++ src/app/certifications-slider/certifications-slider.component.ts
@@ -11,17 +11,50 @@ import { CommonModule } from '@angular/common';
 })
 export class CertificationsSliderComponent implements OnInit {
   certifications = [
-    { src: 'assets/paper/certificatmern.webp', alt: "Software Developer Bootcamp", title: "Software Developer Bootcamp MERN Certificate", description: "2024" },
-    { src: 'assets/paper/Diplome  National  d\'Ingenieur.webp', alt: "National Engineering Diploma", title: "National Engineering Diploma", description: "2019" },
-    { src: 'assets/paper/EPI- Prix d\'excellence PFE.webp', alt: "EPI- Prix d'excellence PFE", title: "EPI - Excellence Award for Final Year Project", description: "2019" },
-    { src: 'assets/paper/diplome en electronique électrotechnique et automatisme.webp', alt: "Diploma in Electronics, Electrotechnics, and Automation", title: "Diploma in Electronics, Electrotechnics, and Automation", description: "2015" },
-    { src: 'assets/paper/diplome baccalauréat.webp', alt: "High School Diploma", title: "Bachelor's degree", description: "2012" },
-    { src: 'assets/paper/attestation de travail Skillware.webp', alt: "Work Certificate from Skillware", title: "Work Certificate from Skillware", description: "" },
-    { src: 'assets/paper/attestation  de Stage EPI.webp', alt: "Internship Certificate from EPI", title: "Internship Certificate from EPI", description: "" },
-    { src: "assets/paper/attestation de Stage Peek Square.webp", alt: "Internship Certificate from Peek Square", title: "Internship Certificate from Peek Square", description: "" },
-    { src: 'assets/paper/attestation de Stage FITELEC.webp', alt: "Internship Certificate from FITELEC", title: "Internship Certificate from FITELEC", description: "" },
-    { src: 'assets/paper/attestation de stage TUNISIE TELECOM.webp', alt: "Internship Certificate from TUNISIE TELECOM", title: "Internship Certificate from TUNISIE TELECOM", description: "" },
-    { src: 'assets/paper/attestation de Stage STEG.webp', alt: "Internship Certificate from STEG", title: "Internship Certificate from STEG", description: "" },
+    {
+      src: 'assets/paper/certificatmern.webp', alt: "Software Developer Bootcamp", title: "Software Developer Bootcamp MERN Certificate", description: "2024", srcSet: 'path-to-image-small.jpg 480w, path-to-image-medium.jpg 1024w, path-to-image-large.jpg 1600w',
+      sizes: '(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw',
+    },
+    {
+      src: 'assets/paper/Diplome  National  d\'Ingenieur.webp', alt: "National Engineering Diploma", title: "National Engineering Diploma", description: "2019", srcSet: 'path-to-image-small.jpg 480w, path-to-image-medium.jpg 1024w, path-to-image-large.jpg 1600w',
+      sizes: '(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw',
+    },
+    {
+      src: 'assets/paper/EPI- Prix d\'excellence PFE.webp', alt: "EPI- Prix d'excellence PFE", title: "EPI - Excellence Award for Final Year Project", description: "2019", srcSet: 'path-to-image-small.jpg 480w, path-to-image-medium.jpg 1024w, path-to-image-large.jpg 1600w',
+      sizes: '(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw',
+    },
+    {
+      src: 'assets/paper/diplome en electronique électrotechnique et automatisme.webp', alt: "Diploma in Electronics, Electrotechnics, and Automation", title: "Diploma in Electronics, Electrotechnics, and Automation", description: "2015", srcSet: 'path-to-image-small.jpg 480w, path-to-image-medium.jpg 1024w, path-to-image-large.jpg 1600w',
+      sizes: '(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw',
+    },
+    {
+      src: 'assets/paper/diplome baccalauréat.webp', alt: "High School Diploma", title: "Bachelor's degree", description: "2012",
+      sizes: '(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw',
+    },
+    {
+      src: 'assets/paper/attestation de travail Skillware.webp', alt: "Work Certificate from Skillware", title: "Work Certificate from Skillware", description: "",
+      sizes: '(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw',
+    },
+    {
+      src: 'assets/paper/attestation  de Stage EPI.webp', alt: "Internship Certificate from EPI", title: "Internship Certificate from EPI", description: "",
+      sizes: '(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw',
+    },
+    {
+      src: "assets/paper/attestation de Stage Peek Square.webp", alt: "Internship Certificate from Peek Square", title: "Internship Certificate from Peek Square", description: "",
+      sizes: '(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw',
+    },
+    {
+      src: 'assets/paper/attestation de Stage FITELEC.webp', alt: "Internship Certificate from FITELEC", title: "Internship Certificate from FITELEC", description: "",
+      sizes: '(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw',
+    },
+    {
+      src: 'assets/paper/attestation de stage TUNISIE TELECOM.webp', alt: "Internship Certificate from TUNISIE TELECOM", title: "Internship Certificate from TUNISIE TELECOM", description: "",
+      sizes: '(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw',
+    },
+    {
+      src: 'assets/paper/attestation de Stage STEG.webp', alt: "Internship Certificate from STEG", title: "Internship Certificate from STEG", description: "",
+      sizes: '(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw',
+    },
   ]
     ;
 
